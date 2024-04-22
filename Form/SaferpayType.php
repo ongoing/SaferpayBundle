6d38b5eeca9d0a6619b6bd64fa9314c028d4df30
kevinthalmann <kevin.thalmann@gmail.com>
2024-04-22 14:09:11 -0300

    [UPDATE] render agb accepted form fields

--- Form/SaferpayType.php
+++ Form/SaferpayType.php
@@ -2,7 +2,10 @@
 
 namespace Ongoing\Payment\SaferpayBundle\Form;
 
+use OMS\InsertionBundle\Validator\Constraints\NotBlank;
+use Ongoing\Payment\SaferpayBundle\Plugin\SaferpayPlugin;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
@@ -14,10 +17,33 @@ class SaferpayType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('agbAccepted', CheckboxType::class, [
+                'label' => /** @Desc("Ich bestätige, die <a href=""https://www.xdate.ch/de/agb"" target=""_blank"">AGB</a> gelesen zu haben und akzeptiere diese. Zudem bestätige ich dass ich über 18 Jahre alt bin.") */ 'form.agbaccepted',
+                'constraints' => [
+                    new NotBlank(
+                        message: /** @Desc("Bitte akzeptieren Sie unsere AGBs.") */ 'form.agb.shouldaccept',
+                        groups: ['saferpay']
+                    )
+                ],
+                'required' => false,
+                'label_attr' => [ 'class' => 'agb-label checkbox-custom']
+            ])
+            ->add('agbAcceptedCrypto', CheckboxType::class, [
+                'label' => /** @Desc("Ich bestätige, die <a href=""https://www.xdate.ch/de/agb"" target=""_blank"">AGB</a> gelesen zu haben und akzeptiere diese. Zudem bestätige ich dass ich über 18 Jahre alt bin.") */ 'form.agbaccepted',
+                'constraints' => [
+                    new NotBlank(
+                        message: /** @Desc("Bitte akzeptieren Sie unsere AGBs.") */ 'form.agb.shouldaccept',
+                        groups: ['saferpay']
+                    )
+                ],
+                'required' => false,
+                'label_attr' => [ 'class' => 'agb-label checkbox-custom']
+            ]);
     }
 
     public function getBlockPrefix()
     {
-        return 'saferpay_checkout';
+        return SaferpayPlugin::PAYMENT_SYSTEM_NAME;
     }
 }
